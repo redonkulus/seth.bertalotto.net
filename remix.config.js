f39c3ad434b5f9e114e1272b7edb13543581f74b
redonkulus <seth@bertalotto.net>
2022-04-18 17:39:29 -0700

    ci: add vercel config

--- remix.config.js
+++ remix.config.js
@@ -7,5 +7,10 @@ module.exports = {
     devServerPort: 8002,
     ignoredRouteFiles: ['.*'],
     publicPath: '/build/',
+    // When running locally in development mode, we use the built in remix
+    // server. This does not understand the vercel lambda module format,
+    // so we default back to the standard build output.
+    server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
     serverBuildDirectory: 'build',
+    serverBuildTarget: 'vercel',
 };
