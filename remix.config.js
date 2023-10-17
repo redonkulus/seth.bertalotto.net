6ac3df2a54b0ec38d74e9d2f608e41013cba6522
redonkulus <seth@bertalotto.net>
2023-10-16 18:01:04 -0700

    feat: upgrade to remix v2

--- remix.config.js
+++ remix.config.js
@@ -2,15 +2,14 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-    // appDirectory: 'app',
-    // assetsBuildDirectory: 'public/build',
-    devServerPort: 8002,
     ignoredRouteFiles: ['.*'],
-    // publicPath: '/build/',
     // When running locally in development mode, we use the built in remix
     // server. This does not understand the vercel lambda module format,
     // so we default back to the standard build output.
     server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-    // serverBuildDirectory: 'build',
-    serverBuildTarget: 'vercel',
+    serverBuildPath: "api/index.js",
+    serverMainFields: ["main", "module"],
+    serverModuleFormat: "cjs",
+    serverPlatform: "node",
+    serverMinify: false,
 };
